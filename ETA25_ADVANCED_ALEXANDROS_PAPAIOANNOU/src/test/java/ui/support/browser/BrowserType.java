9d5a6b93392f04a99c0875b650b2ac5aae147b6f
alexandros-papaioannou <alexandros.papaioannou90@gmail.com>
2025-10-06 14:55:50 +0300

    Flatten repo.

--- ETA25_ADVANCED_ALEXANDROS_PAPAIOANNOU/src/test/java/ui/support/browser/BrowserType.java
+++
@@ -1,8 +0,0 @@
-package ui.support.browser;
-
-public class BrowserType {
-
-    public static final String BROWSER_CHROME = "chrome";
-    public static final String BROWSER_EDGE = "edge";
-    public static final String BROWSER_FIREFOX = "firefox";
-}
--- ETA25_ADVANCED_ALEXANDROS_PAPAIOANNOU/src/test/java/ui/support/utils/extent/ReportStep.java
+++
@@ -1,8 +0,0 @@
-package ui.support.utils.extent;
-
-public class ReportStep {
-
-    public static String INFO_STEP = "info";
-    public static String PASS_STEP = "pass";
-    public static String ERROR_STEP = "error";
-}
